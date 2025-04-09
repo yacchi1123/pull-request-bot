2cd68720f6bc4e2b02d943c6884af9b8e69c63ed
yamasakayuma <yamasaka1123@gmail.com>
2025-04-09 16:31:59 +0900

    test-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <h1>Hello World</h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
